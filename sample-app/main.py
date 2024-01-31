579dc69c4643357122da08691218dc2b0cdc2d2c
Ameenah Burhan <ameenah.burhan@gmail.com>
2024-01-31 13:16:04 -0500

    Update main.py

--- sample-app/main.py
+++ sample-app/main.py
@@ -9,6 +9,7 @@ sample_data = [
         "last_name": "Doe",
         "birthdate": "1980-01-01",
         "homeaddress": "123 Main Street",
+        "country": "US",
         "work_email": "john.doe@example.com",
         "employee_id": "123456",
         "role": "Software Engineer"
@@ -18,6 +19,7 @@ sample_data = [
         "last_name": "Doe",
         "birthdate": "1985-02-02",
         "homeaddress": "456 Elm Street",
+        "country": "US",
         "work_email": "jane.doe@example.com",
         "employee_id": "654321",
         "role": "Manager"
@@ -27,6 +29,7 @@ sample_data = [
         "last_name": "Smith",
         "birthdate": "1990-03-03",
         "homeaddress": "789 Oak Street",
+        "country": "UK",
         "work_email": "bob.smith@example.com",
         "employee_id": "987654",
         "role": "Intern"
